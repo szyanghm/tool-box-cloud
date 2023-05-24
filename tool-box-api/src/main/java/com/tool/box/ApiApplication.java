e91826b3fb5fe9b6ce1a13114c4f0d779fcb5712
v_haimiyang <v_haimiyang@tencent.com>
2023-05-24 16:20:44 +0800

    代码优化

--- tool-box-api/src/main/java/com/tool/box/ApiApplication.java
+++ tool-box-api/src/main/java/com/tool/box/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToolBoxApiApplication {
+public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToolBoxApiApplication.class, args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
