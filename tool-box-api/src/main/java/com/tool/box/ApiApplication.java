e9c02e1116e7dfe172fa4f25d8819e5609d24c26
v_haimiyang <v_haimiyang@tencent.com>
2023-05-24 17:29:45 +0800

    代码优化

--- tool-box-api/src/main/java/com/tool/box/ApiApplication.java
+++ tool-box-api/src/main/java/com/tool/box/ApiApplication.java
@@ -2,7 +2,11 @@ package com.tool.box;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@ServletComponentScan
 @SpringBootApplication
 public class ApiApplication {
 
