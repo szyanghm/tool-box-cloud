19c5bd10453c97fe6c92846b8ecc7065595aa2a6
v_haimiyang <v_haimiyang@tencent.com>
2023-05-24 16:19:11 +0800

    代码优化

---
+++ tool-box-service/src/main/java/com/tool/box/ServiceApplication.java
@@ -0,0 +1,18 @@
+package com.tool.box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@ServletComponentScan
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
