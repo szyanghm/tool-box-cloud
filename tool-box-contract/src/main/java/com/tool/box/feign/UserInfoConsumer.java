896540615c66e6f28aeecbf8ceb2c6da7c4ab20f
v_haimiyang <v_haimiyang@tencent.com>
2023-08-11 20:31:46 +0800

    web提交

--- tool-box-contract/src/main/java/com/tool/box/feign/UserInfoConsumer.java
+++ tool-box-contract/src/main/java/com/tool/box/feign/UserInfoConsumer.java
@@ -1,7 +1,8 @@
 package com.tool.box.feign;
 
-import com.tool.box.base.UserInfo;
+import com.tool.box.base.LoginUser;
 import com.tool.box.decode.NotBreakerConfiguration;
+import com.tool.box.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +19,8 @@ import java.util.Set;
 @FeignClient(name = "tool-box-service", path = "/user", configuration = NotBreakerConfiguration.class)
 public interface UserInfoConsumer {
 
-    @PostMapping(value = "/getUser")
-    UserInfo getUserInfo(@RequestParam("account") String account);
+    @PostMapping(value = "/getLoginUser")
+    LoginUser getLoginUser(@RequestParam("account") String account);
 
     @PostMapping(value = "/getPermissions")
     Set<String> getPermissions(@RequestParam("role") String role);
