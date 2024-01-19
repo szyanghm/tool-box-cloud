f3d92f58d4d363c589f653bba22583403b5fea66
v_haimiyang <v_haimiyang@tencent.com>
2024-01-19 15:38:58 +0800

    加密工具类优化

--- tool-box-common/src/main/java/com/tool/box/utils/DruidEncryptUtils.java
+++ tool-box-common/src/main/java/com/tool/box/utils/DruidEncryptUtils.java
@@ -1,7 +1,10 @@
 package com.tool.box.utils;
 
 import com.alibaba.druid.filter.config.ConfigTools;
+import com.tool.box.enums.SystemCodeEnum;
+import com.tool.box.exception.InternalApiException;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -13,61 +16,62 @@ import java.security.NoSuchProviderException;
  * @Date 2023/4/19 15:43
  * @Version 1.0
  */
+@Slf4j
 public class DruidEncryptUtils {
 
     /**
      * 私钥
      */
-    private static String privateKey;
+    private static final String privateKey;
 
     /**
      * 公钥
      */
-    private static String publicKey;
+    private static final String publicKey;
 
     static {
         try {
             String[] keyPair = ConfigTools.genKeyPair(512);
             privateKey = keyPair[0];
-            System.out.println(String.format("privateKey-->%s",privateKey));
+            log.info("privateKey-->{}", privateKey);
             publicKey = keyPair[1];
-            System.out.println(String.format("publicKey-->%s",publicKey));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchProviderException e) {
-            e.printStackTrace();
+            log.info("publicKey-->{}", publicKey);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
+            log.error(e.getMessage());
+            throw new InternalApiException(SystemCodeEnum.PASSWORD_ENCRYPT_FAILED);
         }
     }
 
     /**
      * 明文加密
-     * @param plaintext
-     * @return
+     *
+     * @param plaintext 明文字符串
+     * @return 加密后
      */
     @SneakyThrows
-    public static String encode(String plaintext){
-        System.out.println("明文字符串：" + plaintext);
-        String ciphertext = ConfigTools.encrypt(privateKey,plaintext);
-        System.out.println("加密后字符串：" + ciphertext);
+    public static String encode(String plaintext) {
+        log.info("明文字符串：{}", plaintext);
+        String ciphertext = ConfigTools.encrypt(privateKey, plaintext);
+        log.info("加密后字符串：{}", ciphertext);
         return ciphertext;
     }
 
     /**
      * 解密
-     * @param ciphertext
-     * @return
+     *
+     * @param ciphertext 加密字符串
+     * @return 解码结果
      */
     @SneakyThrows
-    public static String decode(String ciphertext){
-        System.out.println("加密字符串：" + ciphertext);
-        String plaintext = ConfigTools.decrypt(publicKey,ciphertext);
-        System.out.println("解密后的字符串：" + plaintext);
-
+    public static String decode(String ciphertext) {
+        log.info("加密字符串：{}", ciphertext);
+        String plaintext = ConfigTools.decrypt(publicKey, ciphertext);
+        log.info("解密后的字符串：{}", plaintext);
         return plaintext;
     }
 
     public static void main(String[] args) {
-        String a="123456";
+        String a = "123456";
         String encode = DruidEncryptUtils.encode(a);
         System.out.println("encode =" + encode);
         String decode = DruidEncryptUtils.decode(encode);
