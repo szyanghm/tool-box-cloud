7b0a41d321f9d3e40e3c1bf16a184f64da4a3a42
v_haimiyang <v_haimiyang@tencent.com>
2024-03-14 18:00:32 +0800

    代码优化

--- tool-box-task/src/main/java/com/tool/box/aspect/WebLogAspect.java
+++ tool-box-task/src/main/java/com/tool/box/aspect/WebLogAspect.java
@@ -1,10 +1,22 @@
 package com.tool.box.aspect;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.crypto.SecureUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Maps;
+import com.tool.box.common.Contents;
+import com.tool.box.common.DataStatic;
+import com.tool.box.config.SystemConfig;
+import com.tool.box.enums.SystemCodeEnum;
+import com.tool.box.exception.InternalApiException;
 import com.tool.box.utils.HttpUtils;
+import com.tool.box.utils.RedisUtils;
 import com.tool.box.utils.SystemUtils;
+import com.tool.box.utils.TokenUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -13,8 +25,12 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,21 +45,25 @@ import java.util.Map;
 @Slf4j
 public class WebLogAspect {
 
-    ThreadLocal<Long> startTime = new ThreadLocal<Long>();
-    Map params = Maps.newLinkedHashMap();
+    @Resource
+    private RedisUtils redisUtils;
+    @Resource
+    private TokenUtils tokenUtils;
+    @Resource
+    private SystemConfig systemConfig;
 
+    ThreadLocal<Long> startTime = new ThreadLocal<Long>();
+    Map<Object, Object> params = Maps.newLinkedHashMap();
 
     /**
-     * 以 controller 包下定义的所有请求为切入点
+     * 以 controller 包下定义的所有请求为切入点,排除扫描定时任务控制器
      */
     @Pointcut("execution(public * com.tool.box.controller..*.*(..)) && !within(com.tool.box.controller.JobController)")
     public void webLog() {
     }
 
     /**
-     * 在切点之前织入
-     *
-     * @param joinPoint
+     * 在切点之前植入
      */
     @Before("webLog()")
     public void doBefore(JoinPoint joinPoint) {
@@ -51,31 +71,31 @@ public class WebLogAspect {
         startTime.set(System.currentTimeMillis());
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
                 .getRequestAttributes();
+        assert attributes != null;
         HttpServletRequest request = attributes.getRequest();
-        params.put("【url】", request.getRequestURL()); // 获取请求的url
+        String url = request.getRequestURL().toString();
+        params.put("【url】", url); // 获取请求的url
         params.put("【method】", request.getMethod()); // 获取请求的方式
         params.put("【ip】", HttpUtils.getIpAddr(request)); // 获取请求的ip地址
         params.put("【className】", joinPoint.getSignature().getDeclaringTypeName()); // 获取类名
         params.put("【classMethod】", joinPoint.getSignature().getName()); // 获取类方法
         for (Object object : joinPoint.getArgs()) {
-            if (
-                    object instanceof MultipartFile
-                            || object instanceof HttpServletRequest
-                            || object instanceof HttpServletResponse
-            ) {
+            if (object instanceof MultipartFile
+                    || object instanceof HttpServletRequest
+                    || object instanceof HttpServletResponse) {
                 continue;
             }
             params.put("【request args】", object); // 请求参数
         }
+        //校验重复提交
+        verifyResubmit(url, joinPoint, request);
     }
 
     /**
-     * 在切点之后织入
-     *
-     * @throws Throwable
+     * 在切点之后植入
      */
     @After("webLog()")
-    public void doAfter() throws Exception {
+    public void doAfter() {
         // 输出格式化后的json字符串
         try {
             log.info(JSONObject.toJSONString(params));
@@ -91,9 +111,7 @@ public class WebLogAspect {
     /**
      * 环绕
      *
-     * @param proceedingJoinPoint
-     * @return
-     * @throws Throwable
+     * @return 参数
      */
     @Around("webLog()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -103,9 +121,55 @@ public class WebLogAspect {
             params.put("【spend time】",
                     (System.currentTimeMillis() - startTime.get()) + "ms"); // 响应回包参数
             startTime.remove();
-        } catch (Exception ignore) {
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new InternalApiException(SystemCodeEnum.SYSTEM_BUSY);
         }
         return result;
+    }
+
+    /**
+     * 防抖校验重复提交
+     * 只有带保存和更新权限的接口才进行校验
+     *
+     * @param url       请求接口地址
+     * @param joinPoint aop入参
+     * @param request   http请求体
+     */
+    private void verifyResubmit(String url, JoinPoint joinPoint, HttpServletRequest request) {
+        if (!url.contains("login") && CollectionUtil.isNotEmpty(params)) {
+            Method method = SystemUtils.getCurrentMethod(joinPoint);
+            //当需要新增和更新权限的时候才需要进行校验
+            RequiresPermissions permissions = method.getAnnotation(RequiresPermissions.class);
+            if (ObjectUtil.isNull(permissions)) {
+                return;
+            }
+            String[] arr = permissions.value();
+            List<String> list = Arrays.asList(arr);
+            //获取token
+            String token = request.getHeader(Contents.X_ACCESS_TOKEN);
+            //获取当前运行环境
+            String profile = systemConfig.getEnvironment().getRequiredProperty(Contents.PROFILE);
+            if (!systemConfig.enabled && DataStatic.profileDataList.contains(profile)
+                    && StringUtils.isBlank(token)) {
+                token = tokenUtils.getAuthToken(Contents.ADMIN);
+            }
+            synchronized (this) {
+                //通过token获取上一次的参数
+                String backParams = redisUtils.get(token);
+                //当前请求参数
+                String currentParams = SecureUtil.md5(JSONObject.toJSONString(params));
+                //参数对比
+                if (!currentParams.equals(backParams)
+                        && (list.contains(Contents.OP_WRITE_ADD) || list.contains(Contents.OP_WRITE_UPDATE))) {
+                    //将token作为key,缓存请求参数
+                    redisUtils.set(token, currentParams, 2L);
+                } else {
+                    //参数重复表示重复提交
+                    throw new InternalApiException(SystemCodeEnum.SYSTEM_BUSY_RESUBMIT);
+                }
+            }
+        }
     }
 }
 
